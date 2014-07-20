0de0fefbf54da693f2ea78eaae0c45c7134aa87b
Renato Mascarenhas <mascarenhas.renato@gmail.com>
2014-07-20 00:22:08 -0300

    Implement the Merge-Sort algorithm.

--- shared/utils.h
+++ shared/utils.h
@@ -1,6 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <limits.h>
+#ifndef LONG_MIN
+#  define <linux/limits.h>
+#endif
+
+#include <errno.h>
+
 /* General purpose functions */
 
 extern void pexit(char const *id);
